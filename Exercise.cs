ef1939a9b58cdf5b2f5d3c648f2ce57312705ebe
Dimitar Milushev <deadlycookye@gmail.com>
2021-12-15 15:26:29 +0200

    exercise 3-1

---
+++ Exercise.cs
@@ -0,0 +1,5 @@
+namespace Algorithms {
+    interface Exercise {
+        void calculate();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace Algorithms // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Exercise positiveNegative = new PositiveNegative_Algo();
+            positiveNegative.calculate(); 
+        }
+    }
+}
